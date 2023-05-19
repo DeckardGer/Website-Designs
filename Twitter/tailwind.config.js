d530577e4e12d2fe02f67f0df68b0f5c898397ad
Deckard Gerritsen <deckardger@gmail.com>
2023-05-19 21:45:54 +0800

    Day 1: Completed left navbar and most of right nav

---
+++ Twitter/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                white: "#E1E4E5",
+            },
+        },
+    },
+    plugins: [],
+};
